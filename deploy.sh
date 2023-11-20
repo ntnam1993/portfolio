35163706e4fa517cf74dc350f679645cb45500b7
Nam Nguyen <nam@gotoblink.com>
2023-11-20 19:14:12 +0700

    upadte

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b master
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://ntnam1993.github.io
+# git push -f git@github.ntnam1993:ntnam1993/ntnam1993.github.io.git main
+
+# if you are deploying to https://ntnam1993.github.io/portfolio.git
+ git push -f git@github.ntnam1993:ntnam1993/portfolio.git master
+
+cd -
